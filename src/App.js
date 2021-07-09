9a35a1f38f3415928be80ce05137eacfa666dcdb
Peter Wang <65141087+peterw712@users.noreply.github.com>
2021-07-09 00:00:54 -0400

    header and navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import About from './components/About';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import About from "./components/About";
 
 function App() {
-  return (
-    <div>
-      <main>
-        <About></About>
-      </main>
-    </div>
-  );
-}
+	const [currentTab, setCurrentTab] = useState("about");
+
+	const renderTab = () => {
+		switch (currentTab) {
+			case "about":
+				return <About />;
+		}
+	};
+
+	return (
+		<div>
+			<div className="mobile-header">
+				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+			</div>
+			<div>
+				<main>{renderTab()}</main>
+			</div>
+		</div>
+	);
+};
 
 export default App;
